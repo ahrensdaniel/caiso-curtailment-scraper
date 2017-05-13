128edc07eda98cf848606fd051448627370a03f6
Daniel Ahrens <ahrens.danielj@gmail.com>
2017-05-12 18:41:01 -0700

    Removing unnecessary files

--- readctmtpdf.py
+++
@@ -1,8 +0,0 @@
-import pdftables_api
-from numpy import *
-
-
-c = pdftables_api.Client('5q0tm9aktlp5');
-c.csv('ctmtpdf/pdf/mar/Wind_SolarReal-TimeDispatchCurtailmentReportMar01_2017.pdf', 
-	'ctmtpdf/csv/mar/Wind_SolarReal-TimeDispatchCurtailmentReportMar01_2017.csv');
-
